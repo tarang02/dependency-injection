13d1e0c6063e49f46ddd5eeaf26451270f2b8954
TarangGupta <taranggupta@fico.com>
2022-09-09 15:20:44 +0530

    Primary Beans

--- src/main/java/tarang/springframework/dependencyinjection/controlllers/MyController.java
+++ src/main/java/tarang/springframework/dependencyinjection/controlllers/MyController.java
@@ -1,12 +1,18 @@
 package tarang.springframework.dependencyinjection.controlllers;
 
 import org.springframework.stereotype.Controller;
+import tarang.springframework.dependencyinjection.services.GreetingService;
 
 @Controller
 public class MyController {
 
-    public String sayHello(){
-        System.out.println("inside sayHello");
-        return("Hello");
+    private final GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String getGreeting(){
+        return greetingService.sayGreeting();
     }
 }
---
+++ src/main/java/tarang/springframework/dependencyinjection/services/PrimaryGreetingServiceImpl.java
@@ -0,0 +1,14 @@
+package tarang.springframework.dependencyinjection.services;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Primary
+@Service
+public class PrimaryGreetingServiceImpl implements GreetingService{
+
+    @Override
+    public String sayGreeting() {
+        return "Hello World ----- PRIMARY Bean";
+    }
+}
