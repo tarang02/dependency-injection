1848a403a7e7231c14da86f49bb51eb7397a6eca
TarangGupta <taranggupta@fico.com>
2022-09-12 15:32:08 +0530

    Profiles

---
+++ src/main/java/tarang/springframework/dependencyinjection/controlllers/I18nController.java
@@ -0,0 +1,19 @@
+package tarang.springframework.dependencyinjection.controlllers;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import tarang.springframework.dependencyinjection.services.GreetingService;
+
+@Controller
+public class I18nController {
+
+    private final GreetingService greetingService;
+
+    public I18nController(@Qualifier("i18nService") GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String sayHello(){
+        return greetingService.sayGreeting();
+    }
+}
